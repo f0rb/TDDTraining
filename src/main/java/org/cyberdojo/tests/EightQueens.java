81920a384d89ec94d71b02a8d6fff383c723ba3d
袁臻 <f0rb@163.com>
2018-07-23 15:19:51 +0800

    Eight Queens

---
+++ src/main/java/org/cyberdojo/tests/EightQueens.java
@@ -0,0 +1,57 @@
+package org.cyberdojo.tests;
+
+import java.util.*;
+
+import static java.lang.Math.abs;
+
+/**
+ * EightQueens
+ * <p>
+ * Place eight chess queens on an 8x8 chessboard so that
+ * no two queens threaten each other. Thus, a solution
+ * requires that no two queens share the same row,
+ * column, or diagonal.
+ * <p>
+ * [source: http://en.wikipedia.org/wiki/Eight_queens_puzzle]
+ *
+ * @author f0rb on 2018-07-22.
+ */
+public class EightQueens {
+
+
+    public static final int QUEENS_COUNT = 8;
+
+    public static List<int[]> execute() {
+
+        int[] result = new int[QUEENS_COUNT];
+
+        List<int[]> results = new ArrayList<>();
+        executeNext(results, result, 0, new LinkedHashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7)));
+        return results;
+
+    }
+
+    private static void executeNext(List<int[]> results, int[] result, int index, Set<Integer> candidateNumberSet) {
+        if (index == result.length) {
+            results.add(Arrays.copyOf(result, result.length));
+        } else {
+            for (Integer i : candidateNumberSet) {
+                if (isPositionAvailable(result, index, i)) {
+                    result[index] = i;
+                    Set<Integer> leftSet = new LinkedHashSet<>(candidateNumberSet);
+                    leftSet.remove(i);
+                    executeNext(results, result, index + 1, leftSet);
+                }
+            }
+        }
+    }
+
+    private static boolean isPositionAvailable(int[] result, int index, int position) {
+        for (int i = 0; i < index; i++) {
+            if (index - i == abs(position - result[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/cyberdojo/tests/EightQueensTest.java
@@ -0,0 +1,25 @@
+package org.cyberdojo.tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * EightQueensTest
+ *
+ * @author f0rb on 2018-07-21.
+ */
+public class EightQueensTest {
+    @Test
+    public void test_first_result() {
+        //assertArrayEquals(new int[]{1, 3, 5, 7, 2, 4, 8, 6}, EightQueens.execute().get(0));
+        List<int[]> results = EightQueens.execute();
+        assertArrayEquals(new int[]{0, 4, 7, 5, 2, 6, 1, 3}, results.get(0));
+        assertEquals(92, results.size());
+        results.stream().map(Arrays::toString).forEach(System.out::println);
+    }
+}
