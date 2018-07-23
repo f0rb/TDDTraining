c796f82632c354425c41d8f8fcbdbeea2df1cd83
袁臻 <f0rb@163.com>
2018-07-23 15:18:51 +0800

    Count Coins

---
+++ src/main/java/org/cyberdojo/tests/CountCoins.java
@@ -0,0 +1,47 @@
+package org.cyberdojo.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CountCoins
+ * <p>
+ * There are four types of common coins in US currency:
+ * quarters (25 cents)
+ * dimes (10 cents)
+ * nickels (5 cents)
+ * pennies (1 cent)
+ * <p>
+ * There are 6 ways to make change for 15 cents:
+ * A dime and a nickel;
+ * A dime and 5 pennies;
+ * 3 nickels;
+ * 2 nickels and 5 pennies;
+ * A nickel and 10 pennies;
+ * 15 pennies.
+ * <p>
+ * How many ways are there to make change for a dollar
+ * using these common coins? (1 dollar = 100 cents).
+ * <p>
+ * [Source http://rosettacode.org]
+ *
+ * @author f0rb on 2018-07-20.
+ */
+public class CountCoins {
+
+    public static List<int[]> count(int total) {
+        List<int[]> result = new ArrayList<>();
+        int quarters;
+        int nickles;
+        int dimes;
+        for (quarters = 0; quarters <= total / 25; quarters++) {
+            for (nickles = 0; nickles <= (total - quarters * 25) / 10; nickles++) {
+                for (dimes = 0; dimes <= (total - quarters * 25 - nickles * 10) / 5; dimes++) {
+                    result.add(new int[]{total - quarters * 25 - nickles * 10 - dimes * 5, dimes, nickles, quarters});
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/org/cyberdojo/tests/CountCoinsTest.java
@@ -0,0 +1,66 @@
+package org.cyberdojo.tests;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * CountCoinsTest
+ *
+ * @author f0rb on 2018-07-20.
+ */
+public class CountCoinsTest {
+
+    @Test
+    public void test_1_cent() {
+        // given 1
+        int i = 1;
+
+        // then 1 combinations
+        assertEquals(1, CountCoins.count(i).size());
+    }
+
+    //@Test
+    //public void test_2_cents() {
+    //    // given 1
+    //    int i = 2;
+    //
+    //    // then 1 combinations
+    //    assertEquals(1, CountCoins.count(i).size());
+    //}
+    //
+    @Test
+    public void test_6_9_cents() {
+        int input = 8;
+        assertEquals(2, CountCoins.count(input).size());
+        for (int[] solution : CountCoins.count(input)) {
+            checkSolution(input, solution);
+        }
+    }
+
+    @Test
+    public void test_10_24_cents() {
+        int input = 15;
+        for (int[] solution : CountCoins.count(input)) {
+            checkSolution(input, solution);
+        }
+        assertEquals(6, CountCoins.count(input).size());
+    }
+
+    @Test
+    public void test_100_cents() {
+        int input = 100;
+        for (int[] solution : CountCoins.count(input)) {
+            checkSolution(input, solution);
+        }
+        assertEquals(242, CountCoins.count(input).size());
+    }
+
+    private void checkSolution(int input, int[] solution) {
+        System.out.println(Arrays.toString(solution));
+        int count = solution[3] * 25 + solution[2] * 10 + solution[1] * 5 + solution[0];
+        assertEquals(input, count);
+    }
+}
