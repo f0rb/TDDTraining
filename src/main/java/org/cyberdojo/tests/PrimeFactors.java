e90fe1d8475f172be6e9d457d479e608def2fb7b
袁臻 <f0rb@163.com>
2018-07-31 12:42:47 +0800

    Prime Factors

---
+++ src/main/java/org/cyberdojo/tests/PrimeFactors.java
@@ -0,0 +1,69 @@
+package org.cyberdojo.tests;
+
+import java.util.*;
+
+/**
+ * PrimeFactors
+ *
+ * @author f0rb on 2018-07-23.
+ */
+public class PrimeFactors {
+    private static Set<Integer> primeSet = new TreeSet<>(Arrays.asList(2, 3, 5, 7));
+
+    public static List<Integer> process(int input) {
+        if (input < 0) {
+            List<Integer> results = new LinkedList<>();
+            results.add(-1);
+            results.addAll(process(-input));
+            return results;
+        } else if (input == 0 || input == 1) {
+            return Arrays.asList();
+        } else if (isPrime(input)) {
+            return Arrays.asList(input);
+        }
+
+        int firstFactor;
+        for (firstFactor = 2; firstFactor < input; firstFactor++) {
+            if (isPrime(firstFactor) && isFactor(input, firstFactor)) {
+                break;
+            }
+        }
+
+        List<Integer> results = new LinkedList<>();
+        results.add(firstFactor);
+        results.addAll(process(input / firstFactor));
+        return results;
+    }
+
+    private static boolean isFactor(int number, int factor) {
+        return number % factor == 0;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 0) {
+            number = -number;
+        }
+
+        addMissingPrime(number);
+
+        if (!primeSet.contains(number)) {
+            for (Integer prime : primeSet) {
+                if (isFactor(number, prime)) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    private static void addMissingPrime(int number) {
+        int biggestPrimeForNow = (int) primeSet.toArray()[primeSet.size() - 1];
+        double sqrt = Math.sqrt(number);
+        for (int i = biggestPrimeForNow + 1; i <= sqrt; i++) {
+            if (isPrime(i)) {
+                primeSet.add(i);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/cyberdojo/tests/PrimeFactorsTest.java
@@ -0,0 +1,56 @@
+package org.cyberdojo.tests;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * PrimeFactorsTest
+ *
+ * @author f0rb on 2018-07-23.
+ */
+public class PrimeFactorsTest {
+    @Test
+    public void test_prime_factorize() {
+        assertEquals(Arrays.asList(2), PrimeFactors.process(2));
+        assertEquals(Arrays.asList(3), PrimeFactors.process(3));
+        assertEquals(Arrays.asList(2, 2), PrimeFactors.process(4));
+        assertEquals(Arrays.asList(2, 3), PrimeFactors.process(6));
+        assertEquals(Arrays.asList(2, 2, 3), PrimeFactors.process(12));
+        assertEquals(Arrays.asList(3, 5), PrimeFactors.process(15));
+
+        assertEquals(Arrays.asList(2, 2, 5, 5), PrimeFactors.process(100));
+
+        assertEquals(Arrays.asList(), PrimeFactors.process(1));
+        assertEquals(Arrays.asList(), PrimeFactors.process(0));
+        assertEquals(Arrays.asList(-1), PrimeFactors.process(-1));
+        assertEquals(Arrays.asList(-1, 2, 3), PrimeFactors.process(-6));
+
+        assertEquals(Arrays.asList(19, 19), PrimeFactors.process(361));
+
+    }
+
+
+
+    @Test
+    public void test_is_prime() {
+        assertFalse(PrimeFactors.isPrime(361));
+
+        assertTrue(PrimeFactors.isPrime(2));
+        assertTrue(PrimeFactors.isPrime(3));
+        assertTrue(PrimeFactors.isPrime(5));
+        assertTrue(PrimeFactors.isPrime(11));
+        assertTrue(PrimeFactors.isPrime(37));
+
+        assertFalse(PrimeFactors.isPrime(4));
+        assertFalse(PrimeFactors.isPrime(15));
+
+        for (int i = 2; i < 100; i++) {
+            System.out.println(i + " is " + (PrimeFactors.isPrime(i) ? "" : "not " ) + "a prime number!");
+        }
+    }
+}
