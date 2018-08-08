5f700eea0fdf7b6459bce1babba042ddfa36330f
袁臻 <f0rb@163.com>
2018-08-08 10:08:09 +0800

    FizzBuzz.java

---
+++ src/main/java/org/cyberdojo/tests/FizzBuzz.java
@@ -0,0 +1,21 @@
+package org.cyberdojo.tests;
+
+/**
+ * FizzBuzz
+ *
+ * @author f0rb on 2018-08-01.
+ */
+public class FizzBuzz {
+    public static String of(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+        return i + "";
+    }
+}
---
+++ src/test/java/org/cyberdojo/tests/FizzBuzzTest.java
@@ -0,0 +1,22 @@
+package org.cyberdojo.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * FizzBuzzTest
+ *
+ * @author f0rb on 2018-08-01.
+ */
+public class FizzBuzzTest {
+
+    @Test
+    public void test_succeed() {
+        assertEquals("Fizz", FizzBuzz.of(3));
+        assertEquals("FizzBuzz", FizzBuzz.of(30));
+        assertEquals("Buzz", FizzBuzz.of(5));
+        assertEquals("2", FizzBuzz.of(2));
+    }
+
+}
