e95407607ba1792089afa0c17b8d5b5ab749a463
袁臻 <f0rb@163.com>
2018-08-11 15:30:57 +0800

    kotlin私有化构造函数

--- src/main/java/org/cyberdojo/tests/ClosestToZero.kt
+++ src/main/java/org/cyberdojo/tests/ClosestToZero.kt
@@ -2,20 +2,11 @@ package org.cyberdojo.tests
 
 import kotlin.math.abs
 
-class ClosestToZero {
-
+class ClosestToZero private constructor() {
     companion object {
         fun pick(numbers: IntArray): Int {
             var ret = Integer.MAX_VALUE
             var pos = -1
-
-//            for ((index, number) in numbers.withIndex()) {
-//                println("the element at $index is $number")
-//                if (abs(number) < abs(ret) || number == abs(ret)) {
-//                    ret = number
-//                    pos = index
-//                }
-//            }
 
             numbers.forEachIndexed { index, it ->
                 if (abs(it) < abs(ret) || it == abs(ret)) {
