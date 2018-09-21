d0de0a24fd3bef42404ad3c2adafd3c2838fa212
袁臻 <f0rb@163.com>
2018-09-21 13:19:17 +0800

    KmpMatcher kmp字符串匹配算法

---
+++ src/main/java/org/cyberdojo/tests/KmpMatcher.java
@@ -0,0 +1,121 @@
+package org.cyberdojo.tests;
+
+/**
+ * KmpMatcher
+ *
+ * @author f0rb on 2018-09-14.
+ */
+public class KmpMatcher {
+
+    public static int indexOf(String origin, String match) {
+        int i = 0, j = 0;
+
+        int[] next = getNext2(match);
+
+        int len = origin.length();
+        int matchLength = match.length();
+        while (i - j < len - matchLength) {
+            //if (origin.charAt(i) == match.charAt(j)) {
+            //    i++;
+            //    j++;
+            //} else {
+            //    if (j == 0) {
+            //        i++;
+            //    } else {
+            //        j = next[j];
+            //    }
+            //}
+
+            if (j == -1 || origin.charAt(i) == match.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+
+            if (j >= matchLength) {
+                return i - matchLength;
+            }
+
+        }
+
+        return -1;
+    }
+
+    static int[] computeNext(String match) {
+        int[] next = new int[match.length()];
+        next[0] = -1;
+        boolean matching = false;
+        for (int i = 1; i < next.length; i++) {
+            if (matching) {
+                if (match.charAt(i) != match.charAt(next[i - 1] + 1)) {
+                    matching = false;
+                }
+                next[i] = next[i - 1] + 1;
+            } else if (match.charAt(i) == match.charAt(0)) {
+                matching = true;
+            }
+
+        }
+
+        return next;
+    }
+
+    static int[] getNext(String match) {
+        char[] p = match.toCharArray();
+        int[] next = new int[match.length()];
+
+        next[0] = -1;
+
+        int i = 0, j = -1;
+
+        while (i != match.length() - 1) {
+            //这里注意，i==0的时候实际上求的是next[1]的值，以此类推
+            if (j == -1 || p[i] == p[j]) {
+                ++i;
+                ++j;
+                next[i] = j;
+            } else {
+                j = next[j];
+            }
+
+            //if (j == -1 || origin.charAt(i) == match.charAt(j)) {
+            //    i++;
+            //    j++;
+            //} else {
+            //    j = next[j];
+            //}
+        }
+
+        return next;
+    }
+
+    static int[] getNext2(String match) {
+
+        char[] pattern = match.toCharArray();
+        int[] next = new int[match.length()];
+
+        next[0] = -1;
+
+
+        int k = -1, j = 0;
+
+        while (j < pattern.length - 1) {
+            while (k != -1 && pattern[k] != pattern[j]) {
+                k = next[k];
+            }
+            ++j;
+            ++k;
+            if (pattern[k] == pattern[j]) {
+                next[j] = next[k];
+            } else{
+                next[j] = k;
+            }
+
+        }
+
+        return next;
+
+    }
+
+}
---
+++ src/test/java/org/cyberdojo/tests/KmpMatcherTest.java
@@ -0,0 +1,51 @@
+package org.cyberdojo.tests;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * KmpMatcherTest
+ *
+ * @author f0rb on 2018-09-14.
+ */
+public class KmpMatcherTest {
+    @Test
+    void testIndexOf() {
+        assertEquals(2, KmpMatcher.indexOf("23234235678234567", "23423"));
+        assertEquals(-1, KmpMatcher.indexOf("23234235678234567", "dfghj"));
+        assertEquals(3, KmpMatcher.indexOf("sssssssss34", "ssssss3"));
+        assertEquals(6, KmpMatcher.indexOf("sssssssss34ss", "sss34s"));
+        assertEquals(8, KmpMatcher.indexOf("sssss23a234234234ssss34ss", "234234234"));
+    }
+
+    @Test
+    void testComputeNext() {
+        assertArrayEquals(new int[] {-1, 0, 0}, KmpMatcher.computeNext( "234"));
+        //assertArrayEquals(new int[] {-1, 0, 0, 1}, KmpMatcher.computeNext( "2342"));
+        assertArrayEquals(new int[] {-1, 0, 0, 0}, KmpMatcher.computeNext( "2342"));
+        assertArrayEquals(new int[] {-1, 0, 0, 0, 1}, KmpMatcher.computeNext( "23423"));
+        assertArrayEquals(new int[] {-1, 0, 0, 0, 1, 2}, KmpMatcher.computeNext( "234234"));
+        //ssssss1
+        // ssssss1
+        assertArrayEquals(new int[] {-1, 0, 1, 2, 3, 4, 5}, KmpMatcher.computeNext( "ssssss1"));
+        //assertArrayEquals(new int[] {-1, 0, 1, 2, 3, 4, 5}, KmpMatcher.computeNext( "ssssss1ss"));
+        System.out.println(Arrays.toString(KmpMatcher.computeNext("ssssss1ss")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext("ssssss1ss")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext2("ssssss1ss")));
+        System.out.println();
+        System.out.println(Arrays.toString(KmpMatcher.computeNext("234234234")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext("234234234")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext2("234234234")));
+        System.out.println();
+        System.out.println(Arrays.toString(KmpMatcher.computeNext("asdfas234")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext("asdfas234")));
+        System.out.println(Arrays.toString(KmpMatcher.getNext2("asdfas234")));
+
+
+    }
+}
