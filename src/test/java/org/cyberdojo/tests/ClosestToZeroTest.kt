6b1e5f1ddcba163ce304187154646ae9bfd64b0a
袁臻 <f0rb@163.com>
2018-11-05 16:57:21 +0800

    kotlin

--- src/test/java/org/cyberdojo/tests/ClosestToZeroTest.kt
+++ src/test/java/org/cyberdojo/tests/ClosestToZeroTest.kt
@@ -1,9 +1,15 @@
 package org.cyberdojo.tests
 
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
-public class ClosestToZeroTest {
+/**
+ * ClosestToZeroTest
+ *
+ * @author f0rb
+ * @date 2018-10-14
+ */
+class ClosestToZeroTest {
     @Test
     public fun test_closest_number() {
         assertEquals(1, ClosestToZero.pick(intArrayOf(1, 3, -2, 4)))
